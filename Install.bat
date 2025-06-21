eaf510ba3ac3470612bdabc10aaf28655aa6c7fd
Daniel <74920220+Kurisu003@users.noreply.github.com>
2025-06-21 13:02:15 +0200

    added install file

---
+++ Install.bat
@@ -0,0 +1,7 @@
+@echo off
+python -m pip install pillow
+
+echo.
+echo All required modules have been installed.
+echo Press any key to close this window...
+pause >nul
--- bmp_conv.py
+++ bmp_conv.py
@@ -3,10 +3,6 @@ from PIL import Image
 img_name = input("Image name: \n")
 use_dither = input("\nUse dithering for gray areas? (y/n): \n").lower() == 'y'
 
-if(not use_dither):
-    thr = input("\nThershold: \n")
-
-
 # Load the image
 image = Image.open(img_name)
 
